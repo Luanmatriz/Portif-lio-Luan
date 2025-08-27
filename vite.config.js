1a1465fb9cc8be32a053ad5abb4ea84447cfccc0
Luanmatriz <luanmattriz@gmail.com>
2025-08-27 01:34:12 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,5 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  // 👇 coloque o nome exato do seu repositório no GitHub aqui
-  base: '/Portif-lio-Luan/',
+  base: '/Portif-lio-Luan/', // <== adiciona essa linha
   plugins: [react()],
-});
