36f7840c0daf7e641dfcccc051ea94deee53be2c
Luanmatriz <luanmattriz@gmail.com>
2025-08-27 02:31:19 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
+import Portfolio from './Portfolio.jsx';
 import './index.css';
-import PortfolioPremium from './Portfolio.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HashRouter>
-      <PortfolioPremium />
-    </HashRouter>
+    <Portfolio />
   </React.StrictMode>
 );
